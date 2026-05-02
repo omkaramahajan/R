2248f2b483285f67eda6476916a5ef6bdd592463
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2026-05-02 19:34:06 +0530

    Add files via upload

---
+++ slip6.r
@@ -0,0 +1,22 @@
+marks <- c(45, 70, 90, 66, 89) 
+subjects <- c("M-I", "S-I", "C-I", "C-II", "IKS") 
+barplot(marks, 
+        names.arg = subjects, 
+        main = "Marks of 5 Subjects", 
+        xlab = "Subjects", 
+        ylab = "Marks", 
+        col = "skyblue", 
+        border = "blue") 
+text(x = seq_along(marks), y = marks, label = marks, pos = 3)
+
+hours <- c(5,7,3,8,6,9) 
+score <- c(80,85,60,90,75,95) 
+plot(hours, score, 
+     main = "Study Hours vs Exam Score", 
+     xlab = "Study Hours", 
+     ylab = "Exam Score", 
+     col = "red", 
+     pch = 19) 
+grid()
+corr_val <- cor(hours, score)  
+cat("Correlation between Study Hours and Exam Score:", corr_val, "\n")
