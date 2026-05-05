51a8271ceb81e5b07ea3bac8faa8294d83787d83
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2026-05-05 10:26:09 +0530

    Add files via upload

---
+++ slip8.r
@@ -0,0 +1,18 @@
+v1 <- c(1, 2, 3, 4, 2, 3) 
+v2 <- c("A", "B", "C", "D", "B", "C") 
+df <- data.frame(v1, v2) 
+print(df) 
+dup_rows <- df[duplicated(df), ] 
+cat("Duplicate Rows:\n") 
+print(dup_rows) 
+unique_rows <- unique(df) 
+cat("Unique Rows:\n") 
+print(unique_rows)
+
+x <- c(1.8, 1.9, 2.5, 1.4, 1.3, 2.1, 2.3) 
+y <- c(200, 270, 450, 160, 90, 440, 380) 
+model <- lm(y ~ x) 
+cat("Regression Equation:\n") 
+print(coef(model)) 
+predicted_y <- predict(model, data.frame(x = 2)) 
+cat("Estimated y when x = 2:", predicted_y, "\n")
